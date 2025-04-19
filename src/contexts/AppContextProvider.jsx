3f45d4e070c2db676448429a682cb4bb942117f6
Owless <120806328+Owless@users.noreply.github.com>
2025-04-19 21:55:16 +0300

    Update AppContextProvider.jsx

--- src/contexts/AppContextProvider.jsx
+++ src/contexts/AppContextProvider.jsx
@@ -4,9 +4,14 @@ import { AppContext, useAppState } from './AppContext';
 export const AppProvider = ({ children }) => {
   const appState = useAppState();
   
-  return (
-    <AppContext.Provider value={appState}>
-      {children}
-    </AppContext.Provider>
-  );
-};
+return (
+  <AppContext.Provider value={{ 
+    user, 
+    setUser, 
+    isLoading,
+    isDevMode,
+    refreshUserData // Добавьте эту функцию
+  }}>
+    {children}
+  </AppContext.Provider>
+);
