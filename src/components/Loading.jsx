c5f42a8fa2f451fcbb2223c1cc7bd6b9f8ed5306
Owless <120806328+Owless@users.noreply.github.com>
2025-04-21 13:10:32 +0300

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+// Импортируем SyncLoader
+import SyncLoader from "react-spinners/SyncLoader"; 
 
-const Loading = () => {
+const LoadingPriceTracker = () => {
+  const loaderColor = "#4f46e5"; // Фиолетовый цвет
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
-      <div className="relative w-20 h-20">
-        <div className="absolute top-0 left-0 w-full h-full border-4 border-gray-200 rounded-full"></div>
-        <div className="absolute top-0 left-0 w-full h-full border-4 border-t-purple-800 rounded-full animate-spin"></div>
-      </div>
-      <p className="mt-6 text-lg font-medium text-gray-800">Загрузка данных...</p>
+      <SyncLoader
+        color={loaderColor}
+        loading={true}
+        size={15} // Размер точек
+        margin={5} // Расстояние между точками
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      <p className="mt-6 text-lg font-medium text-gray-800">Обновление цен...</p> 
     </div>
   );
 };
 
-export default Loading;
+export default LoadingPriceTracker;
