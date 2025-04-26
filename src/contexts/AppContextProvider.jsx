c04f8a3df56b864a2b30406b50e946752971b780
Owless <120806328+Owless@users.noreply.github.com>
2025-04-26 23:28:06 +0300

    Update AppContextProvider.jsx

--- src/contexts/AppContextProvider.jsx
+++ src/contexts/AppContextProvider.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { AppContext } from './AppContext';
-import { useAppState } from './useAppState';
 
 export const AppProvider = ({ children }) => {
-  const appState = useAppState();
   return (
     <AppContext.Provider value={appState}>
       {children}
