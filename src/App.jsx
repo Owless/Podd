366d0b9ef842882c5fe3d1b660950bce7bf50bd0
Owless <120806328+Owless@users.noreply.github.com>
2025-04-26 18:52:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider } from './contexts/AppContextProvider';
 import HomePage from './pages/HomePage';
+import ReferralsPage from './pages/ReferralsPage';
+import ProfilePage from './pages/ProfilePage';
+import BottomNavigation from './components/BottomNavigation';
 
 function App() {
   return (
