b1e56c6f50468de68aeafa4d069fb8a2dcd61ffb
Owless <120806328+Owless@users.noreply.github.com>
2025-04-26 23:15:28 +0300

    Update AppContextProvider.jsx

--- src/contexts/AppContextProvider.jsx
+++ src/contexts/AppContextProvider.jsx
@@ -1,26 +1,12 @@
-// src/contexts/AppContextProvider.jsx
 import React from 'react';
 import { AppContext } from './AppContext';
+import { useAppState } from './useAppState';
 
 export const AppProvider = ({ children }) => {
   const appState = useAppState();
-  
   return (
-    <AppContext.Provider value={{...appState, refreshUserData}}>
+    <AppContext.Provider value={appState}>
       {children}
     </AppContext.Provider>
   );
-};
-
-// Добавляем функцию обновления данных
-const refreshUserData = async (setUser) => {
-  try {
-    const response = await fetch('/api/user/info');
-    if (response.ok) {
-      const data = await response.json();
-      setUser(data.user);
-    }
-  } catch (error) {
-    console.error('Refresh error:', error);
-  }
 };
