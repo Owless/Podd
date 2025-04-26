1856cb63df386e920334dd92ba52d239295a13af
Owless <120806328+Owless@users.noreply.github.com>
2025-04-27 00:08:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
 import './App.css';
 import HomePage from './pages/HomePage';
 import ReferralsPage from './pages/ReferralsPage';
 import ProfilePage from './pages/ProfilePage';
 import BottomNavigation from './components/BottomNavigation';
-import { AppProvider } from './contexts/AppContextProvider'; // с фигурными скобками!
-import ErrorModal from './components/ErrorMessage';
+import { AppProvider } from './contexts/AppContextProvider';
 
 function App() {
   return (
-    <AppProvider> {/* используешь AppProvider */}
+    <AppProvider>
       <Router>
         <div className="app min-h-screen bg-gray-100">
           <Routes>
